49878abd40f191b6868e51030ea579d6a131a548
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-20 22:03:52 +0530

    added bootstrap-jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,25 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// import the bootstrap javascript module
+import "bootstrap"
+
+// import the application.scss we created for the bootstrap CSS (if you are not using assets stylesheet)
+import "../stylesheets/application"
+
+var moment = require('moment')
+
+var jQuery = require('jquery')
+
+import "daterangepicker"
+
+// include jQuery in global and window scope (so you can access it globally)
+// in your web browser, when you type $('.div'), it is actually refering to global.$('.div')
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+// include moment in global and window scope (so you can access it globally)
+global.moment = moment;
+window.moment = moment;
+
