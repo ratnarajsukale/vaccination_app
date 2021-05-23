74e009da98c54fc4cb122ddc0b997300dfb029f2
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-23 21:16:51 +0530

    added error messages for editing hospitals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  add_flash_types :danger, :info, :warning, :success
+
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :age)}
--- app/controllers/hospitals_controller.rb
+++ app/controllers/hospitals_controller.rb
@@ -3,8 +3,8 @@ class HospitalsController < ApplicationController
 
   def authenticate_staff
     unless current_user and current_user.staff?
-      redirect_to root_path
-      flash.now[:notice] = "you are not auth"
+      redirect_to root_path, danger: "You are not authenticated user"
+       
     end
   end
 
