2c6941d0c547bce76d45366833b4d56cd260319e
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-25 21:12:24 +0530

    added bookings CRUD

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one :booking
+
+  def already_booked_slot
+    Booking.where(user_id: id).exists?
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#index"
   resources :hospitals
+  resources :hospitals do
+    resources :bookings, only: [:new, :create] 
+  end
+
+  resources :bookings, only: [:show, :edit, :create, :destroy, :update]
   
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  
+
+  
 end
