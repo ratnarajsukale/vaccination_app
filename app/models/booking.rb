aebafc69205c93b6c1c7911cf533dd180e556998
Ratnaraj Sukale <rvsukale@gmail.com>
2021-05-26 17:31:19 +0530

    validated date presence

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :hospital
   validate :date_cannot_be_in_the_past
+  validates :date, presence: true
 
   def date_cannot_be_in_the_past
     if date.present? && date < Date.today
