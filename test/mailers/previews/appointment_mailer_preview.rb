be779d487d2cd7bc5ec4daadb4cc3351b1f403db
Ratnaraj Sukale <rvsukale@gmail.com>
2021-06-01 20:45:44 +0530

    Removed minitest

--- test/mailers/previews/appointment_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/appointment_mailer
-class AppointmentMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/appointment_mailer/appointment_scheduled
-  def appointment_scheduled
-    AppointmentMailer.with(user: User.first).appointment_scheduled
-  end
-
-end
