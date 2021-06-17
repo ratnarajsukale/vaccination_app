28b6e58ec3ebe2faaf13b12e28e6df6a288f3f55
Ratnaraj Sukale <rvsukale@gmail.com>
2021-06-17 12:45:25 +0530

    completed signin_workflow spec

--- spec/features/signin_workflow_spec.rb
+++ spec/features/signin_workflow_spec.rb
@@ -19,7 +19,11 @@ RSpec.feature "SigninWorkflows", type: :feature do
       expect(page).to have_content 'Welcome! You have signed up successfully.'
     end
 
-    
+    scenario "should not be successful with short password" do
+      fill_in 'user_password', with: '12345'
+      fill_in 'user_password_confirmation', with: '12345'
+      click_button 'Sign up'
+      expect(page).to have_content 'Password is too short (minimum is 6 characters)'
+    end
   end
-
 end
