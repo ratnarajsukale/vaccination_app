85bbc463a576cf1e2460810d35b463ed71cb82b6
Ratnaraj Sukale <rvsukale@gmail.com>
2021-06-17 10:40:02 +0530

    added first feature test

---
+++ spec/features/signin_workflow_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.feature "SigninWorkflows", type: :feature do
+  context 'create new user' do 
+    before(:each) do
+      visit '/users/sign_up'
+      within("form") do
+        fill_in 'user_name', with: 'john'
+        fill_in 'user_age', with: '22'
+        fill_in 'user_email', with: 'akash@example.com'
+        fill_in 'user_password', with: '222222'
+        fill_in 'user_password_confirmation', with: '222222'
+
+      end
+    end
+
+    scenario "should be successful" do
+      click_button 'Sign up'
+      expect(page).to have_content 'Welcome! You have signed up successfully.'
+    end
+
+    
+  end
+
+end
